e9c4bf84229e9515735a63292ae319e62941ed2f
annieopyoke <annieopyoke@gmail.com>
2017-11-25 18:38:03 -0500

    retirementage class test

--- retirementage_test.rb
+++ retirementage_test.rb
@@ -5,4 +5,8 @@ class Testretirementage < Minitest::Test
 	def test_assert_that_1_equal_1
 	assert_equal(1, 1) 
 	end
+
+	def test_assert_that_retirementage_is_an_array
+		assert_equal(Array, retirementageArClass())
+	end
 end
