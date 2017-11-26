64a21c0829bd2176bcf20ca0a4b1fafbca5702ce
annieopyoke <annieopyoke@gmail.com>
2017-11-25 19:40:48 -0500

    wombat test fail

--- retirementage.rb
+++ retirementage.rb
@@ -8,3 +8,16 @@ def retirementageClass(test_number)
 	test_number.class
 end
 
+def counting(test_number)
+	retirementage = test_number
+	25.times do 
+		$retirementageAr.push(retirementage)
+		retirementage += test_number
+	end
+
+	puts $retirementageAr
+	if $retirementageAr == (Array)
+		true
+	else false
+	end
+end
